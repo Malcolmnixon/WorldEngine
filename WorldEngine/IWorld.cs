a741b785f387e2c8fcf41d5d87e39cb7a4d7aea0
Malcolm Nixon <malcolm.nixon@gmail.com>
2019-11-22 21:24:28 -0500

    Initial Commit

---
+++ WorldEngine/IWorld.cs
@@ -0,0 +1,6 @@
+﻿namespace WorldEngine
+{
+    public interface IWorld
+    {
+    }
+}
---
+++ WorldEngineTest/LocalWorldTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorldEngine;
+
+namespace WorldEngineTest
+{
+    [TestClass]
+    public class LocalWorldTest
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var world = new World();
+            Assert.IsNotNull(world);
+        }
+    }
+}
